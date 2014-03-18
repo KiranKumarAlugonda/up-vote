f2c8ae58c7d182d45b4f01a1f9c0d79e4eb71c12
ldeavila <luis@ideanerd.com>
2014-03-17 22:44:05 -0700

    Cleaned up code and formatting

--- public/app/app.js
+++ public/app/app.js
@@ -1,45 +1,44 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute', 'uvNavbar', 'topicsModule']).config(['$routeProvider', function ($routeProvider) {
+angular.module('app', ['ngRoute', 'uvNavbar', 'topicsModule']).config(['$routeProvider', function ($routeProvider) {
 
-	/** Routes **/
-	$routeProvider.when('/', {
-		templateUrl: '/app/common/main.html',
-		controller: 'mainController'
-	});
+		/** Routes **/
+		$routeProvider.when('/', {
+			templateUrl: '/app/common/main.html',
+			controller: 'mainController'
+		});
 
-	$routeProvider.when('/topics', {
-		templateUrl: '/app/components/topics/topics.html',
-		controller: 'topicsController'
-	});
+		$routeProvider.when('/topics', {
+			templateUrl: '/app/components/topics/topics.html',
+			controller: 'topicsController'
+		});
 
-	$routeProvider.when('/topics/add', {
-		templateUrl: '/app/components/topics/topics-add.html',
-		controller: 'topicsAddController'
-	});
+		$routeProvider.when('/topics/add', {
+			templateUrl: '/app/components/topics/topics-add.html',
+			controller: 'topicsAddController'
+		});
 
-	$routeProvider.otherwise('/');
-}]);
+		$routeProvider.otherwise('/');
+	}])
 
 /**  Main Controller **/
-app.controller('mainController', ['$scope', function ($scope) {
-	// Namespace object for variables and functions
-	$scope.mainControllerNS = {};
+	.controller('mainController', ['$scope', function ($scope) {
+		// Namespace object for variables and functions
+		$scope.mainControllerNS = {};
 
-	// variables
-	$scope.mainControllerNS.title = 'Up Vote!';
+		// variables
+		$scope.mainControllerNS.title = 'Up Vote!';
 
-}]);
+	}])
 
 /** Footer Controller **/
-app.controller('footerController', ['$scope', function ($scope) {
-	// Namespace object for variables and functions
-	$scope.footerControllerNS = {};
+	.controller('footerController', ['$scope', function ($scope) {
+		// Namespace object for variables and functions
+		$scope.footerControllerNS = {};
 
-	// variables
-	//TODO: Set these as variables and constants so we have examples of those things
-	$scope.footerControllerNS.version = '0.2';
-	$scope.footerControllerNS.builtIn = 'L.A.';
-	$scope.footerControllerNS.builtBy = 'Idea Nerd';
+		// variables
+		//TODO: Set these as variables and constants so we have examples of those things
+		$scope.footerControllerNS.builtIn = 'L.A.';
+		$scope.footerControllerNS.builtBy = 'Idea Nerd';
 
-}]);
+	}]);
